65522f2601992dc1bf1d3985a37b742e24433268
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-04-05 13:22:43 -0300

    feat: adds JwtModule import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import 'dotenv/config';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      signOptions: {
+        expiresIn: '60s',
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategyService],
 })
 export class AuthModule {}
