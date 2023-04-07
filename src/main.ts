3f25b9277a3298012438a5ab53c57dd01e69c1bd
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-04-07 16:10:35 -0300

    config: adds validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3002);
 }
 bootstrap();
