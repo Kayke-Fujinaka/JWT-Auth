68db104427a1c72379a0f98a6681dadcf1e0bf30
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-04-07 16:23:05 -0300

    feat: creates routes in auth controller

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { Role } from '../role.decorator';
-import { RoleGuard } from '../role.guard';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDTO } from './dto/login.dto';
 import { JwtGuard } from './jwt.guard';
 
 @Controller('auth')
@@ -9,14 +8,14 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login(@Body() body) {
-    return { token: this.authService.login(body.username, body.password) };
+  login(@Body() { username, password }: LoginDTO) {
+    return this.authService.login(username, password);
   }
 
-  @Role('admin')
-  @UseGuards(JwtGuard, RoleGuard)
+  @UseGuards(JwtGuard)
   @Get('/test-auth')
-  test() {
+  test(@Req() req) {
+    console.log(req.user);
     return { name: 'User' };
   }
 }
